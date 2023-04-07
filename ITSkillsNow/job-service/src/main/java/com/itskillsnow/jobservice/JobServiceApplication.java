d5b304f597c8183b69db5ba307c2cdc5d72dd7ca
Mohammed Aljader <461166@student.fontys.nl>
2023-04-07 17:07:57 +0200

    integrated with azure blob storage

--- ITSkillsNow/job-service/src/main/java/com/itskillsnow/jobservice/JobServiceApplication.java
+++ ITSkillsNow/job-service/src/main/java/com/itskillsnow/jobservice/JobServiceApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-
 @SpringBootApplication
 @EnableDiscoveryClient
 public class JobServiceApplication {
---
+++ ITSkillsNow/job-service/src/main/java/com/itskillsnow/jobservice/service/interfaces/BlobService.java
@@ -0,0 +1,15 @@
+package com.itskillsnow.jobservice.service.interfaces;
+
+
+import java.io.InputStream;
+import java.util.List;
+
+public interface BlobService {
+    List<String> getAllFiles();
+    String downloadFile(String blobItem);
+    Boolean storeFile(String filename, InputStream content, long length);
+
+    boolean deleteFile(String filename);
+
+
+}
