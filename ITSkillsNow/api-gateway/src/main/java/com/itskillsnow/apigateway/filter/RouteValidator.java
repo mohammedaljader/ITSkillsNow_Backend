6b685ccbecacb4e31192aef0c31ed244b1848a18
Mohammed Aljader <461166@student.fontys.nl>
2023-05-15 17:03:49 +0200

    some changes to api gateway

--- ITSkillsNow/api-gateway/src/main/java/com/itskillsnow/apigateway/filter/RouteValidator.java
+++ ITSkillsNow/api-gateway/src/main/java/com/itskillsnow/apigateway/filter/RouteValidator.java
@@ -9,24 +9,40 @@ import java.util.function.Predicate;
 @Component
 public class RouteValidator {
 
+    // URL: /api/ServiceName/open/**
     public static final List<String> openApiEndpoints = List.of(
             "/auth/register",
             "/auth/login",
+            "/auth/validate",
             "/eureka"
     );
 
+    // URL: /api/ServiceName/admin/**
     public static final List<String> adminEndpoints = List.of(
             "/api/course",
             "/api/job"
     );
 
+    // URL: /api/ServiceName/company/**
+    public static final List<String> companyEndpoints = List.of(
+            "/api/course",
+            "/api/job"
+    );
+
+    // URL: /api/ServiceName/user/**
     public static final List<String> userEndpoints = List.of(
-            "/api/job/messages"
+            "/api/job/messages",
+            "/auth/deleteMe"
     );
 
 
     public Predicate<ServerHttpRequest> isAdmin =
             request -> adminEndpoints
+                    .stream()
+                    .anyMatch(uri -> request.getURI().getPath().contains(uri));
+
+    public Predicate<ServerHttpRequest> isCompany =
+            request -> companyEndpoints
                     .stream()
                     .anyMatch(uri -> request.getURI().getPath().contains(uri));
 
